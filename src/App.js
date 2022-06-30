933725019cce823cce6424063984afd12832a875
Yoshida.Eiji <yoshida.eiji@trans-cosmos.co.jp>
2022-06-30 17:49:44 +0900

    22:08まで完了

--- src/App.js
+++ src/App.js
@@ -1,52 +1,18 @@
-import Button from "@mui/material/Button";
-import SettingsIcon from "@mui/icons-material/Settings";
-import AddIcon from "@mui/icons-material/Add";
-import Typography from "@mui/material/Typography";
+import { Box, Container, Stack } from "@mui/material";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      hello
-      <Button variant="text">Text</Button>
-      <Button
-        startIcon={<SettingsIcon />}
-        variant="contained"
-        color="secondary"
-        size="small"
-      >
-        SETTINGS
-      </Button>
-      <Button
-        startIcon={<AddIcon />}
-        variant="contained"
-        color="success"
-        size="small"
-      >
-        ADD NEW POST
-      </Button>
-      <Button variant="outlined" disabled>
-        Outlined
-      </Button>
-      <Typography variant="h1">h1. Heading</Typography>
-      <Button
-        variant="contained"
-        disabled
-        sx={{
-          backgroundColor: "skyblue",
-          color: "#888",
-          margin: 5,
-          "&:hover": {
-            backgroundColor: "blue",
-          },
-          "&:disabled": {
-            backgroundColor: "gray",
-            color: "white",
-          },
-        }}
-      >
-        My Unique Button
-      </Button>
-    </div>
+    <Box>
+      {/* navbar */}
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 }
 
