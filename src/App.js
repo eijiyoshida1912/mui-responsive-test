a44ceaa30b45d87b60ed9b0afadbe4f23aceed67
Yoshida.Eiji <yoshida.eiji@trans-cosmos.co.jp>
2022-06-30 16:26:37 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@mui/material/Button";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AddIcon from "@mui/icons-material/Add";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
+      <Button variant="text">Text</Button>
+      <Button
+        startIcon={<SettingsIcon />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        SETTINGS
+      </Button>
+      <Button
+        startIcon={<AddIcon />}
+        variant="contained"
+        color="success"
+        size="small"
+      >
+        ADD NEW POST
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+      <Typography variant="h1">h1. Heading</Typography>
+      <Button
+        variant="contained"
+        disabled
+        sx={{
+          backgroundColor: "skyblue",
+          color: "#888",
+          margin: 5,
+          "&:hover": {
+            backgroundColor: "blue",
+          },
+          "&:disabled": {
+            backgroundColor: "gray",
+            color: "white",
+          },
+        }}
+      >
+        My Unique Button
+      </Button>
     </div>
   );
 }
